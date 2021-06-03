8a3fd734a8681a952dd0df7960beb39c1fe3356e
hijiki02 <rebanira02@gmail.com>
2021-06-03 15:24:11 +0900

    Add GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon Callbacks
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+    #endregion
+
+
+    #region Public Methods
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    #endregion
+}
