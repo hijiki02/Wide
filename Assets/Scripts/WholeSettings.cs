969c91ac879e3a9136cc051ab248b16bd1b9517f
hijiki02 <rebanira02@gmail.com>
2021-08-26 15:13:59 +0900

    🔖 0.11a: バージョンの変更

--- Assets/Scripts/WholeSettings.cs
+++ Assets/Scripts/WholeSettings.cs
@@ -2,7 +2,7 @@
 public struct WholeSettings
 {
     //ゲームのバージョン
-    public static string GameVersion { get; } = "0.09a";
+    public static string GameVersion { get; } = "0.11a";
 
     //部屋毎の最大プレイヤー数
     public static byte MaxPlayersPerRoom { get; } = 4;
