f79a79b1cbbcb766cca1c72f1dcce7f3e1a8c898
hijiki02 <rebanira02@gmail.com>
2021-09-01 23:04:36 +0900

    💄 ClassroomのUIスケールモードを変更

--- Assets/Scripts/WholeSettings.cs
+++ Assets/Scripts/WholeSettings.cs
@@ -2,7 +2,7 @@
 public struct WholeSettings
 {
     //ゲームのバージョン
-    public static string GameVersion { get; } = "0.20a";
+    public static string GameVersion { get; } = "0.21a";
 
     //部屋毎の最大プレイヤー数
     public static byte MaxPlayersPerRoom { get; } = 4;
