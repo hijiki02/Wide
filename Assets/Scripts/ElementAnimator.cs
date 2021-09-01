ee791ed8c60fd144371bcf32abff2477d4b161fe
hijiki02 <rebanira02@gmail.com>
2021-09-01 20:24:20 +0900

    💫フェードアニメーションを追加

--- Assets/Scripts/ElementAnimator.cs
+++ Assets/Scripts/ElementAnimator.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 public class ElementAnimator : MonoBehaviour
 {
     #region プライベートフィールド
     [Tooltip("移動する座標量")]
     [SerializeField]
-    private Vector3 move = new Vector3(0f, 0f, 0f);
+    private Vector3 trip = new Vector3(0f, 0f, 0f);
 
     [Tooltip("持続秒数")]
     [SerializeField]
@@ -22,21 +24,30 @@ public class ElementAnimator : MonoBehaviour
     #region 変数
     private float delay;
     private Vector3 initialPosition;
+    private Image image;
     #endregion
 
     #region Unityのコールバック
     void Start()
     {
         initialPosition = transform.position;
+        image = GetComponent<Image>();
         delay = delayLevel * WholeSettings.AnimationDelay;
 
-        transform.position = initialPosition - move;
-    }
+        transform.position = initialPosition - trip;
+        image.color = new Func<Color>(() =>
+        {
+            Color color = image.color;
+            color.a = 0f;
+            return color;
+        })();
 
-    void Update()
-    {
         transform
-            .DOMove(initialPosition + move, duration)
+            .DOMove(initialPosition + trip, duration)
+            .SetEase(Ease.OutCubic)
+            .SetDelay(delay);
+
+        image.DOFade(1f, duration)
             .SetEase(Ease.OutCubic)
             .SetDelay(delay);
     }
